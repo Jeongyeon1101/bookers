b3f43c8028d2af5844415fe07572bbd97ef11760
MegumiSaito <twice1101oncemegumi@gmail.com>
2023-07-16 08:51:22 +0000

    [Finish] 4-15

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-     redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
     else
-     @books = Book.all
-     render :index
+      @books = Book.all
+      render :index
     end
   end
 
@@ -23,9 +24,15 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
+
   end
 
   def destroy
